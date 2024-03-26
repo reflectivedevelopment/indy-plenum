a7cd8dcb279cbf1087114b80534f282fe27c24f6
Kim Ebert <kim@indicio.tech>
2024-03-26 09:01:17 -0600

    chore: commit updated work
    Signed-off-by: Kim Ebert <kim@indicio.tech>

--- build-scripts/ubuntu-2204/build-3rd-parties.sh
+++ build-scripts/ubuntu-2204/build-3rd-parties.sh
@@ -25,7 +25,7 @@ function build_rocksdb_deb {
     rm -rf /tmp/rocksdb
 }
 
-function build_from_pypi {
+function build_from_pypi_fpm {
     PACKAGE_NAME=$1
 
     if [ -z $2 ]; then
@@ -87,6 +87,30 @@ function build_from_pypi {
     rm ${PREREM_TMP}
 }
 
+function build_from_pypi_wheel {
+    PACKAGE_NAME=$1
+
+    if [ -z $2 ]; then
+        PACKAGE_VERSION=""
+        # Get the most recent package version from PyPI to be included in the package name of the Debian artifact
+        curl -X GET "https://pypi.org/pypi/${PACKAGE_NAME}/json" > "${PACKAGE_NAME}.json"
+        PACKAGE_VERSION="==$(cat "${PACKAGE_NAME}.json" | jq --raw-output '.info.version')"
+        rm "${PACKAGE_NAME}.json"
+    else
+        PACKAGE_VERSION="==$2"
+    fi
+
+    rm -rvf /tmp/wheel
+    mkdir /tmp/wheel
+    pushd /tmp/wheel
+    pip3 wheel ${PACKAGE_NAME}${PACKAGE_VERSION}
+    wheel2deb
+    mv output/*.deb ${OUTPUT_PATH}
+    popd
+    rm -rvf /tmp/wheel
+}
+
+
 # TODO duplicates list from Jenkinsfile.cd
 SCRIPT_PATH="${BASH_SOURCE[0]}"
 pushd `dirname ${SCRIPT_PATH}` >/dev/null
@@ -97,33 +121,33 @@ pip install -r requirements.txt
 # Build rocksdb at first
 ### Can be removed once the code has been updated to run with rocksdb v. 5.17
 ### Issue 1551: Update RocksDB; https://github.com/hyperledger/indy-plenum/issues/1551
-# build_rocksdb_deb 5.8.8
+#build_rocksdb_deb 5.8.8
 
 #### PyZMQCommand
-#build_from_pypi pyzmq 22.3.0 --zmq=bundled
+build_from_pypi_fpm pyzmq 22.3.0 --zmq=bundled
 
 ##### install_requires
-#build_from_pypi base58
+build_from_pypi_wheel base58
 ### Needs to be pinned to 3.10.1 because from v4.0.0 the package name ends in python3-importlib-metadata_0.0.0_amd64.deb
 ### https://github.com/hyperledger/indy-plenum/runs/4166593170?check_suite_focus=true#step:5:5304
-build_from_pypi importlib-metadata
-build_from_pypi ioflo
-build_from_pypi jsonpickle
-build_from_pypi leveldb
-build_from_pypi libnacl 1.6.1
-build_from_pypi msgpack-python
-build_from_pypi orderedset
-build_from_pypi packaging 21.3
-build_from_pypi portalocker 2.7.0
-build_from_pypi prompt-toolkit 3.0.18
-build_from_pypi psutil
-build_from_pypi pympler 0.8
-build_from_pypi python-dateutil
-build_from_pypi python-rocksdb
-build_from_pypi python-ursa 0.1.1
-build_from_pypi rlp 0.6.0
-build_from_pypi semver 2.13.0
-build_from_pypi sha3
-build_from_pypi six
-build_from_pypi sortedcontainers 1.5.7
-build_from_pypi ujson 1.33
+build_from_pypi_wheel importlib-metadata 3.10.1
+build_from_pypi_wheel ioflo
+build_from_pypi_wheel jsonpickle
+build_from_pypi_wheel leveldb
+build_from_pypi_wheel libnacl 1.6.1
+build_from_pypi_wheel msgpack-python
+build_from_pypi_wheel orderedset
+build_from_pypi_wheel packaging 21.3
+build_from_pypi_wheel portalocker 2.7.0
+build_from_pypi_wheel prompt-toolkit 3.0.18
+build_from_pypi_fpm psutil
+build_from_pypi_wheel pympler 0.8
+build_from_pypi_wheel python-dateutil
+build_from_pypi_wheel python-rocksdb
+build_from_pypi_wheel python-ursa 0.1.1
+build_from_pypi_wheel rlp 0.6.0
+build_from_pypi_wheel semver 2.13.0
+build_from_pypi_wheel sha3
+build_from_pypi_wheel six
+build_from_pypi_wheel sortedcontainers 1.5.7
+build_from_pypi_wheel ujson 1.33
